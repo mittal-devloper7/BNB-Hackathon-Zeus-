f4d863749710acb691328505809ca27045b538f3
Mittal <mittalhalarnekar@gmail.com>
2026-09-11 18:43:08 +0530

    changed some issues in useers.js

--- models/user.js
+++ models/user.js
@@ -1,26 +1,45 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // Associations will be added later
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      role: {
+        type: DataTypes.ENUM("CHILD", "PARENT", "COORDINATOR", "ADMIN"),
+        allowNull: false,
+        defaultValue: "CHILD",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "Users",
+    },
+  );
+
   return User;
 };
